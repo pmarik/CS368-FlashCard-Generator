0f2c3a39e69fdf8d1ebe139dae558e085cb4ab3b
Paul <pmarik@wisc.edu>
2017-05-02 16:37:28 -0500

    Create Flashcard.hpp

---
+++ Flashcard.hpp
@@ -0,0 +1,45 @@
+////////////////////////////////////////////////////////////////////////////////
+// File Name:      Flashcards.hpp
+//
+// Author:         Paul Marik, David Wissink 
+// CS email:       marik@cs.wisc.edu, wissink@cs.wisc.edu
+//
+// Description:    Represenation of the flashcards to be used in the flashcards
+//                 generator.
+//
+// Sources:        cppreference.com, <sources, if any>
+//
+// URL(s) of sources:
+//                 http://en.cppreference.com/w/cpp/container/vector
+//                 <URLs of your sources, if any>
+////////////////////////////////////////////////////////////////////////////////
+
+
+#ifndef Final_Flashcard_HPP
+#define Final_Flashcard_HPP
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+class Flashcard{
+
+    private:
+
+     std::string fileName;
+
+    public:
+        /**
+     * @brief Constructor for flashcards.
+     *
+     * @param string the filename of the user's document to generate cards from.
+     */
+     Flashcard();
+
+
+
+
+};
+
+#endif //Final_Flashcard_HPP
