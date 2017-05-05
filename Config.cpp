592caed3c5715b3da0bd49519422fa3d1d6dfb86
Paul <pmarik@wisc.edu>
2017-05-04 21:37:56 -0500

    Update Config.cpp

--- Config.cpp
+++ Config.cpp
@@ -17,10 +17,12 @@
 
 using namespace std;
 
-
+        /** Configuration presets for the main menu 
+        *
+        **/
         const static string welcome_message = "---------- WELCOME TO FLASHCARD GENERATOR! ----------";
         const static string username_prompt = "Enter your FlashCard Generator username or create a new one: ";
-        const static string yes_or_no = "(Y)es or (N)o: ";
+        const static string yes_or_no = "(Y/N): ";
         const static string menu = "---------- FLASHCARD GENERATOR MENU ----------";
         const static string review_option = "(R)eview Cards";
         const static string edit_option = "(E)dit Decks";
@@ -30,4 +32,4 @@ using namespace std;
         const static string exit_message = "*** Thanks for using FlashCard Generator! ***";
         const static string above_choice = "Please enter an option from those listed above: ";
         const static string no_decks_message = "No decks found! Please add decks to begin a study session!";
-        const static string deck_list_message = "Please choose a deck from your current deck list:";
+        const static string deck_list_message = "Please choose a deck from your current deck list: **";
